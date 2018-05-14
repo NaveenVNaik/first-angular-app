026799e698fb34430d8119ead5a708acd083977e
Naveen <naveennaikhnr@gmail.com>
2018-05-14 23:49:19 +0530

    added inline template servers component to and inline style to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  //we can use inline style as well instead of refering to external file
+  styles:[
+    `
+      h3{
+        color:green;
+      }
+    `
+  ]
 })
 export class AppComponent {
 
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // templateUrl: './servers.component.html',
+  // instead of above templateUrl we can use inline html template as well
+  template: `
+    <h3>Inside servers components inline template</h3>
+    <app-server></app-server>
+    <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
