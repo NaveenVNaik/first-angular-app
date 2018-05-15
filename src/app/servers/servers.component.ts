6615a4479b1bdb67d1daf390c5d31e70d4e683ee
Naveen <naveennaikhnr@gmail.com>
2018-05-15 18:30:53 +0530

    updated from office

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  //using css tag element
   selector: 'app-servers',
+
+  //using css attributes
+  //selector: '[app-servers]',
+
+  //using css class
+  //selector: '.app-servers',
+
   // templateUrl: './servers.component.html',
   // instead of above templateUrl we can use inline html template as well
   template: `
     <h3>Inside servers components inline template</h3>
     <app-server></app-server>
     <app-server></app-server>`,
+
   styleUrls: ['./servers.component.css']
+  
 })
 export class ServersComponent implements OnInit {
 
